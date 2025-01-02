d2936b06a6dbe42c9752c07064240aa7616380e0
Renaud Bellec <renaud.bellec.3@gmail.com>
2025-01-02 21:39:50 +0100

    fix: pdf url

--- src/components/ExportCv.tsx
+++ src/components/ExportCv.tsx
@@ -1,7 +1,7 @@
 export function ExportCv() {
   return (
     <p className="text-primary-500 dark:text-neutral-200 underline text-right">
-      <a href="/resume/public/renaud_bellec_software_dev.pdf" target="_blank">
+      <a href="/resume/renaud_bellec_software_dev.pdf" target="_blank">
         💾 Download this as a .pdf file
       </a>
     </p>
